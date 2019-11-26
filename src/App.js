6e3ac2f6c36bbbd50dbf9a2a6783d67ea339e76c
Lou Huang <lou@louhuang.com>
2019-11-26 15:18:57 -0500

    feat: add google sheets integration

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Grid, Dropdown, Button, Icon, Input } from 'semantic-ui-react'
 import Radar from 'react-d3-radar'
 import find from 'lodash/find'
@@ -7,8 +7,16 @@ import DataInput from './DataInput'
 import { convertUnits } from './utils/conversions'
 import UNITS from './data/units.json'
 import ATTRIBUTES from './data/attributes_numo.json'
-import VEHICLE_PROFILES from './data/vehicle_profiles.json'
 import './App.css'
+
+function getNewVehicleId () {
+  return (
+    'vehicle_' +
+    Math.random()
+      .toString(36)
+      .substr(2, 9)
+  )
+}
 
 function mapAttributeValuesToLevel (attributes) {
   const levels = Object.entries(attributes).reduce((obj, [key, attribute]) => {
@@ -86,18 +94,122 @@ function Attributes ({ values, sendValues = () => {} }) {
   )
 }
 
+const mapToVehicleProfile = ({
+  weight,
+  speed,
+  footprint,
+  emissions,
+  health,
+  ...others
+}) => {
+  return {
+    attributes: { weight, speed, footprint, emissions, health },
+    ...others
+  }
+}
+
 function App () {
   const [values, setValues] = useState({})
-  const [name, setName] = useState('')
+  const [vehicles, setVehicles] = useState([])
+  const [error, setError] = useState('')
+  const [success, setSuccess] = useState('')
+  const [lastUpdate, setLastUpdate] = useState(new Date().toISOString())
+  const [pending, setPending] = useState(false)
+  const [selectedVehicle, setSelectedVehicle] = useState({})
+  const url =
+    'https://lwh6oxm5db.execute-api.us-east-1.amazonaws.com/dev/vehicles'
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(url).catch(err => {
+        if (err) {
+          console.error(err)
+        }
+        setError('Unable to fetch vehicle profiles')
+      })
+
+      if (!result) return
+      const myJson = await result.json()
+
+      const vehicles = myJson.map(mapToVehicleProfile)
+      setVehicles(vehicles)
+    }
+    fetchData()
+  }, [lastUpdate])
+
+  async function createToApi () {
+    const clone = {
+      ...selectedVehicle,
+      key: getNewVehicleId(),
+      text: `${selectedVehicle.text} copy`,
+      value: `${selectedVehicle.text} copy`
+    }
+    setSelectedVehicle(clone)
+    saveToApi('POST', clone)
+  }
+
+  async function updateToApi () {
+    saveToApi('PUT', selectedVehicle)
+  }
+
+  async function saveToApi (method, selected) {
+    setPending(true)
+    console.log('saving', { values, vehicles })
+
+    if (!selected) {
+      console.log('no vehicle selected?')
+      return
+    }
+    const finalVehicle = {
+      key: selected.key,
+      text: selected.text,
+      value: selected.value
+    }
+    const meta = ['id', 'app:edited', 'save', 'del', '_xml']
+    Object.keys(values).forEach(vehicleAttribute => {
+      if (meta.includes(vehicleAttribute)) return
+      finalVehicle[`attributes${vehicleAttribute}`] = values[vehicleAttribute]
+    })
+    console.log({ finalVehicle })
+
+    const result = await fetch(url, {
+      method,
+      body: JSON.stringify({ vehicle: finalVehicle })
+    }).catch(err => {
+      if (err) {
+        console.error(err)
+      }
+      setError('Unable to save vehicle profile')
+      setPending(false)
+    })
+
+    if (!result) return
+    console.log('!!! yes')
+    const { data } = result
+    console.log({ data, result })
+    setLastUpdate(new Date().toISOString())
+    setSuccess('Saved vehicle to google sheets.')
+    setPending(false)
+  }
 
   function sendValues (values) {
     setValues(values)
   }
 
   function handleDropdownChange (event, data) {
-    const vehicle = find(VEHICLE_PROFILES, { value: data.value })
+    const vehicle = find(vehicles, { key: data.value })
     setValues(vehicle.attributes)
-    setName(vehicle.text)
+    console.log({ vehicle })
+    setSelectedVehicle(vehicle)
+  }
+
+  function handleNameChange (event, data) {
+    const newVehicle = {
+      ...selectedVehicle,
+      text: event.target.value,
+      value: event.target.value
+    }
+    console.log({ newVehicle })
+    setSelectedVehicle(newVehicle)
   }
 
   return (
@@ -119,7 +231,11 @@ function App () {
                   fluid
                   search
                   selection
-                  options={VEHICLE_PROFILES}
+                  value={selectedVehicle && selectedVehicle.key}
+                  options={vehicles.map(item => ({
+                    text: item.text,
+                    value: item.key
+                  }))}
                   onChange={handleDropdownChange}
                   style={{
                     margin: 0 /* Override a right margin from Semantic-UI */
@@ -130,9 +246,9 @@ function App () {
                 <label htmlFor="input-name">Vehicle name (optional)</label>
                 <Input
                   id="input-name"
-                  value={name}
+                  value={selectedVehicle && selectedVehicle.text}
                   placeholder="My vehicle"
-                  onChange={event => setName(event.target.value)}
+                  onChange={handleNameChange}
                 />
               </div>
               <Attributes values={values} sendValues={sendValues} />
@@ -191,10 +307,50 @@ function App () {
                   <Icon name="download" />
                   Download vector image (SVG)
                 </Button>
+
+                {!pending && selectedVehicle && selectedVehicle.key && (
+                  <Button
+                    primary
+                    basic
+                    icon
+                    labelPosition="left"
+                    onClick={updateToApi}
+                  >
+                    <Icon name="download" />
+                    Save vehicle attributes
+                  </Button>
+                )}
+                {!pending && selectedVehicle && selectedVehicle.key && (
+                  <Button
+                    primary
+                    basic
+                    icon
+                    labelPosition="left"
+                    onClick={createToApi}
+                  >
+                    <Icon name="download" />
+                    Save as new vehicle
+                  </Button>
+                )}
+                {pending && <div>Updating vehicle data...</div>}
               </div>
             </div>
           </Grid.Column>
         </Grid.Row>
+        {error && (
+          <Grid.Row columns={1}>
+            <Grid.Column>
+              <h2 className="error">{error}</h2>
+            </Grid.Column>
+          </Grid.Row>
+        )}
+        {success && (
+          <Grid.Row columns={1}>
+            <Grid.Column>
+              <h2 className="success">{success}</h2>
+            </Grid.Column>
+          </Grid.Row>
+        )}
       </Grid>
     </div>
   )
