7f660e6963cc4ee0c300de754889e3a9c803ce88
Lou Huang <lou@louhuang.com>
2020-02-01 05:52:22 -0500

    feat(Header): update intro text

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,11 +9,11 @@ function AppHeader () {
         <Header.Subheader>A policy guidance tool by NUMO</Header.Subheader>
       </Header>
       <p>
-        This platform presents guidance towards the level or sustainability of
-        any (existing or hypothetical) vehicle depending on its attributes.
-        There are pre-loaded values that you can choose to learn the policy
-        implications of existing vehicles, and you can also add characteristics
-        for any other vehicle and get a result.
+        This platform helps assess the impact of any vehicle (existing or
+        hypothetical) depending on its attributes. There are pre-loaded values
+        that you can choose to learn the policy implications of existing
+        vehicles. You can add customized characteristics, or create new vehicle
+        types as well.
       </p>
     </>
   )
